babffbadb1570199047be9cb7bef2a7eeb603738
Ajay Singh Chauhan <107171838+AjaySinghChauhan85@users.noreply.github.com>
2025-03-22 16:00:32 +0530

    Update AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,7 +5,7 @@ const AboutPage = () => {
     <>
       <Navbar />
       <div className="container my-3 py-3">
-        <h1 className="text-center">About AjaySinghChauhan</h1>
+        <h1 className="text-center">About Us</h1>
         <hr />
         <p className="lead text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
