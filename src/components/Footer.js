58be4836e6e0baad49231c946d6000c1ab73993b
Ajay Singh Chauhan <107171838+AjaySinghChauhan85@users.noreply.github.com>
2025-05-13 13:45:35 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,8 +6,7 @@ const Footer = () => {
       <footer className="mb-0 text-center">
         <div className="d-flex align-items-center justify-content-center pb-5">
           <div className="col-md-6">
-            <p className="mb-3 mb-md-0">Made with ❤️ by Ajay Singh Chauhan
-              <a  href="https://github.com/AjaySinghChauhan85" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Ajay Singh Chauhan</a>
+           <a  href="https://github.com/AjaySinghChauhan85" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Ajay Singh Chauhan</a>
             </p>
             <a className="text-dark fs-4" href="https://github.com/AjaySinghChauhan85" target="_blank" rel="noreferrer">
               <i className="fa fa-github"></i>
